07676d4ee9a61f95d07944cdb23868ff3da2e409
hurtcraft <abeuhkclefeuh@gmail.com>
2025-11-15 20:31:38 +0100

    merge main

--- BlazorGameClient/obj/Debug/net9.0/BlazorGameClient.AssemblyInfo.cs
+++ BlazorGameClient/obj/Debug/net9.0/BlazorGameClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorGameClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+326a6ae8476cf5aeaf2705a214d086cbc31b0092")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+032297a586a85d562baaf9e676bd3df08b4e5859")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorGameClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorGameClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
